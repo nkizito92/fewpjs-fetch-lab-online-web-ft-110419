68a0e7f1e1ac881f647ea24b96829b92e45aaa4a
Kizito Njoku <nkizito57@gmail.com>
2020-03-06 03:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
+<<<<<<< HEAD
   return fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
   .then(json => getPosts(json));
+=======
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json));
+>>>>>>> bc35d1004bf5c57266d0009a35fcf6e803a631e1
 }
 
 function renderBooks(json) {
@@ -11,6 +17,7 @@ function renderBooks(json) {
     const ul = document.createElement('ul');
     h2.innerHTML = `<h2>${book.name}</h2>`;
     ul.innerHTML = `<li> This book has ${book.numberOfPages} pages. </li>`;
+<<<<<<< HEAD
     main.appendChild(h2);
     main.appendChild(ul);
   });
@@ -18,9 +25,23 @@ function renderBooks(json) {
 
 function getPosts(json) {
   renderBooks(json);
+=======
+    main.appendChild(h2)
+    main.appendChild(ul)
+  })
+>>>>>>> bc35d1004bf5c57266d0009a35fcf6e803a631e1
 }
 
+function getPosts(json) {
+  json.forEach(book => {
+    document.querySelector('title').innerHTML = `${book.name}`;
+  });
+}
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+<<<<<<< HEAD
 });
 
+=======
+});
+>>>>>>> bc35d1004bf5c57266d0009a35fcf6e803a631e1
